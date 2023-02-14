19ea4830987afdb5d5d9d93a6558372eac9c6601
alisontickner <112663174+alisontickner@users.noreply.github.com>
2023-02-14 17:57:42 -0500

    homework 19

---
+++ js/index.js
@@ -0,0 +1,8 @@
+console.log("your index.js file is loaded correctly");
+
+$("button").hover(function(){
+    $(this).css("background-color", "#E6B0A4");
+  },
+  function(){
+    $(this).css("background-color", "#859488");
+  });
